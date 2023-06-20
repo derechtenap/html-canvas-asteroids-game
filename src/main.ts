11fbb03e75463d86f28906db3ff5731dd625a0fe
Tim Deres <50623315+derechtenap@users.noreply.github.com>
2023-06-20 18:06:21 +0200

    Animate player with game loop

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,14 @@ type Coordinates = {
 
 type PlayerType = {
   position: Coordinates;
-  velocity: number;
+  rotation: number;
+  velocity: Coordinates;
 };
 
 const PLAYER_COLOR = "white";
+const PLAYER_SPEED = 2;
+const PLAYER_ROTATIONAL_SPEED = 0.025;
+const PLAYER_FRICTION = 0.97;
 
 const canvas = document.querySelector("canvas");
 
@@ -29,15 +33,14 @@ if (!ctx) {
 canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
 
-// Create a black canvas background
-ctx.fillRect(0, 0, canvas.width, canvas.height);
-
 class Player {
   position: Coordinates;
-  velocity: number;
+  rotation: number;
+  velocity: Coordinates;
 
   constructor({ position, velocity }: PlayerType) {
     this.position = position; // { x, y }
+    this.rotation = 0;
     this.velocity = velocity;
   }
 
@@ -50,6 +53,14 @@ class Player {
 
     ctx.strokeStyle = PLAYER_COLOR;
 
+    ctx.save();
+
+    // Rotating the player relative to canvas center
+    ctx.translate(this.position.x, this.position.y);
+    ctx.rotate(this.rotation);
+    ctx.translate(-this.position.x, -this.position.y);
+
+    ctx.beginPath();
     // Circle inside the player triangle
     ctx.arc(this.position.x, this.position.y, 5, 0, Math.PI * 2);
 
@@ -59,12 +70,86 @@ class Player {
     ctx.lineTo(this.position.x - 10, this.position.y + 10);
     ctx.closePath();
     ctx.stroke();
+    ctx.restore();
+  }
+
+  update() {
+    this.draw();
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
   }
 }
 
 const player = new Player({
   position: { x: canvas.width / 2, y: canvas.height / 2 },
-  velocity: 0,
+  rotation: 0,
+  velocity: { x: 0, y: 0 },
 });
 
-player.draw();
+const keys = {
+  w: {
+    pressed: false,
+  },
+  a: {
+    pressed: false,
+  },
+  d: {
+    pressed: false,
+  },
+};
+
+window.addEventListener("keydown", ({ code: keyCode }) => {
+  switch (keyCode) {
+    case "KeyW":
+    case "ArrowUp":
+      keys.w.pressed = true;
+      break;
+    case "KeyD":
+    case "ArrowRight":
+      keys.d.pressed = true;
+      break;
+    case "KeyA":
+    case "ArrowLeft":
+      keys.a.pressed = true;
+      break;
+  }
+});
+
+window.addEventListener("keyup", ({ code: keyCode }) => {
+  switch (keyCode) {
+    case "KeyW":
+    case "ArrowUp":
+      keys.w.pressed = false;
+      break;
+    case "KeyD":
+    case "ArrowRight":
+      keys.d.pressed = false;
+      break;
+    case "KeyA":
+    case "ArrowLeft":
+      keys.a.pressed = false;
+      break;
+  }
+});
+
+const animate = () => {
+  // Create a new  black canvas background on every frame
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  player.update();
+
+  window.requestAnimationFrame(animate);
+
+  if (keys.w.pressed) {
+    player.velocity.x = Math.cos(player.rotation) * PLAYER_SPEED;
+    player.velocity.y = Math.sin(player.rotation) * PLAYER_SPEED;
+  } else if (!keys.w.pressed) {
+    player.velocity.x *= PLAYER_FRICTION;
+    player.velocity.y *= PLAYER_FRICTION;
+  }
+
+  if (keys.d.pressed) player.rotation += PLAYER_ROTATIONAL_SPEED;
+  if (keys.a.pressed) player.rotation -= PLAYER_ROTATIONAL_SPEED;
+};
+
+animate();
